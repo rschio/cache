8604b3a92733d0dd29d9476d6e79910dee8cb753
Rodrigo Schio <rodrigoschio3@gmail.com>
2019-05-21 14:04:10 -0400

    mutex unexported

--- cache.go
+++ cache.go
@@ -4,8 +4,8 @@ import "sync"
 
 // Cache is a map[string]interface{}.
 type Cache struct {
-	sync.RWMutex
-	m map[string]interface{}
+	mu sync.RWMutex
+	m  map[string]interface{}
 	// max is the maximum number of elements
 	// Cache can store.
 	max int
@@ -26,19 +26,13 @@ func New(max int) *Cache {
 
 // SetMax set a new max value.
 func (c *Cache) SetMax(max int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	if max <= 0 {
 		max = 1
 	}
-	c.Lock()
 	c.max = max
-	c.Unlock()
-
-	c.resize()
-}
-
-func (c *Cache) resize() {
-	c.Lock()
-	defer c.Unlock()
+	// resize cache if necessary.
 	for k := range c.m {
 		if len(c.m) <= c.max {
 			break
@@ -47,39 +41,35 @@ func (c *Cache) resize() {
 	}
 }
 
-func (c *Cache) keepMaxSize() {
-	c.Lock()
-	defer c.Unlock()
-	if len(c.m) < c.max {
-		return
-	}
-
-	for k := range c.m {
-		delete(c.m, k)
-		break
-	}
-}
-
 // Insert stores the element el with key k in the Cache. If the Cache
 // is already full one element is deleted and then store el.
 func (c *Cache) Insert(k string, el interface{}) {
-	c.keepMaxSize()
-	c.Lock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	_, ok := c.m[k]
+	// if there is no k in memory and cache is full,
+	// need to remove one element.
+	if !ok && len(c.m) == c.max {
+		for k := range c.m {
+			delete(c.m, k)
+			break
+		}
+	}
+
 	c.m[k] = el
-	c.Unlock()
 }
 
 // Delete deletes the element of key k from Cache.
 func (c *Cache) Delete(k string) {
-	c.Lock()
+	c.mu.Lock()
 	delete(c.m, k)
-	c.Unlock()
+	c.mu.Unlock()
 }
 
 // Get returns the element of key k and if it is in the Cache.
 func (c *Cache) Get(k string) (interface{}, bool) {
-	c.RLock()
-	defer c.RUnlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	el, ok := c.m[k]
 	return el, ok
 }
@@ -87,8 +77,8 @@ func (c *Cache) Get(k string) (interface{}, bool) {
 // Range range over the Cache and execute the function f
 // for each element.
 func (c *Cache) Range(f func(string, interface{})) {
-	c.RLock()
-	defer c.RUnlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	for key, val := range c.m {
 		f(key, val)
 	}
